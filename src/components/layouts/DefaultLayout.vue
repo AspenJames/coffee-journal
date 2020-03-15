ba4d0feac861a6bb8e4cb5778ee12b0b5b3d95c6
Aspen James <aspenjames@tqca.org>
2020-03-14 20:29:08 -0700

    Add dynamic layout
    Credit for this dynamic layout pattern is due to a blog post by Markus
    Oberlehner: https://markus.oberlehner.net/blog/dynamic-vue-layout-components

---
+++ src/components/layouts/DefaultLayout.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="default">
+    <nav id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <main class="content">
+      <slot />
+    </main>
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "@/components/shared/Footer.vue";
+
+export default {
+  name: "DefaultLayout",
+  components: {
+    Footer
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+#nav
+  padding: 30px
+  background-color: $header-color
+  color: $navlink-color
+  font-size: 1.5em
+  margin: 0
+
+  a
+    font-weight: bold
+    color: $navlink-color
+    &:hover
+      color: $link-color-hover
+
+  a.router-link-exact-active
+    color: $navlink-color-active
+
+main.content
+  padding-bottom: 50px
+  margin: 20px 0 50px
+  height: 100%
+</style>
---
+++ src/components/shared/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer>
+    developed by
+    <a href="https://aspenjames.dev/">Aspen James</a>, licensed under
+    <a
+      href="https://github.com/AspenJames/coffee-journal/blob/master/LICENSE.txt"
+      >GNU GPLv3</a
+    >
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style lang="sass" scoped>
+footer
+  position: fixed
+  left: 0
+  bottom: 0
+  width: 100%
+  background-color: $footer-color
+  color: $text-color-light
+  text-align: center
+  padding: 30px 0
+
+  a
+    color: $navlink-color
+    font-weight: bold
+    text-decoration: none
+    &:hover
+      color: $link-color-hover
+      text-decoration: underline
+</style>
